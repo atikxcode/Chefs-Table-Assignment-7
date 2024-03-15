2e40ed05c2d9fb04de0af23c4b52ae067d72df60
atikxcode <liadbd20@gmail.com>
2024-03-16 04:12:03 +0600

    finished the load data section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Banner from './components/Banner/Banner'
+import Cart from './components/Cart/Cart'
 import Header from './components/Header/Header'
+import Recipes from './components/Recipe/Recipes'
 
 function App() {
 
@@ -8,6 +10,10 @@ function App() {
     <>
      <Header></Header>
      <Banner></Banner>
+     
+     <Recipes></Recipes>
+     
+     
     </>
   )
 }
---
+++ src/components/Recipe/Recipes.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Recipe from "./Recipe";
+import Cart from "../Cart/Cart";
+
+
+const Recipes = () => {
+
+  const [menus, setMenus] = useState([]);
+
+  useEffect(() => {
+    fetch('menu.json')
+    .then(res => res.json())
+    .then(data => setMenus(data))
+  }, [])
+  return (
+    <div className="container mx-auto mb-10">
+      
+      
+      <div className="flex flex-col items-center mb-10">
+        <h2 className="text-[40px] font-semibold mb-6">Out Recipes</h2>
+        <p className="text-center w-[823px] text-[16px] leading-[26px] text-[#150B2B99]">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque. </p>
+      </div>
+     <div className="flex justify-between">
+      <div className="grid grid-cols-2 gap-10">
+      {
+        menus.map(menu => <Recipe key={menu.recipe_id} menu={menu}></Recipe>)
+      }
+      </div>
+      <div>
+      <Cart></Cart>
+      </div>
+     </div>
+
+     
+      
+
+
+    </div>
+    
+  );
+};
+
+export default Recipes;
